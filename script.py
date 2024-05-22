2e5a957152189467f706db0baecbf8523eae381d
saimshamim8423 <144903153+saimshamim8423@users.noreply.github.com>
2024-05-22 21:34:19 +0530

    Update script.py

--- script.py
+++ script.py
@@ -16,12 +16,12 @@ class script(object):
 <b>⦿ /viewthumb - Use This Command To See Your Thumbnail</b>
 <b>⦿ /delthumb - Use This Command To Delete Your Thumbnail</b>"""
 
-    ABOUT_TXT = """<b>🤖 My Name :</b> <a href='https://t.me/FileRenameXProBot'>Rename Bot 4GB</a>
+    ABOUT_TXT = """<b>🤖 My Name :</b> <a href='https://t.me/AnityRenamebot'>Rename Bot 4GB</a>
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a>
-<b>🧑‍💻 Developer :</b> <a href='https://t.me/CallAdminRobot'>Jishu Developer</a>
+<b>📢 Channel :</b> <a href='https://t.me/Anity_managementbots'>AnitySmile Botz</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/Beesmileee'>Smile Developer</a>
 
 <b>♻️ Bot Made By :</b> @Madflix_Bots"""
 
@@ -30,7 +30,7 @@ class script(object):
 
 If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
 
-<b>🛍 UPI ID:</b> <code>madflixofficial@axl</code> 
+<b>🛍 UPI ID:</b> <code>8423047960@ybl</code> 
 
 <b>📸 Send Screenshot After Donation To Activate Your Premium Plan </b> """
 
