0dff3382d7877eb60ff5f68c0fbe49cfdd0b7ddb
saimshamim8423 <144903153+saimshamim8423@users.noreply.github.com>
2024-06-06 20:09:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQFCPbcAuoPQpJtu4A-4Rb9hNa0OafTZAvwdgf8dNUOB_6C2LHzcsHyssheR7AxjoxDdzD_cMUwBaJ-U1IX6jgOxKmuhY5LT4yYeI_XpinTlgHxzp9COLu5qo6UhE5s_LP-Su1ZnO87Fha_hFasdd4NchtstoRvl-GGpNFazPMqXbIFtLKbT8oQ38m3Mi55oA9YZ3RZqaJq0oogtYGqKnh7DJT_Lv6Wef1K83bUOQcpkaqztp6-Y4PPsJ2TXFJZMW1O8aYsGGzomKcIHm69npcHbhFgvYA6rH9_C0orLD01v6PxnRz_U4fEPddEfJn6wwEi22acv9djWlSWL4-fFoVJjOlWfFwAAAAB14Y0AAA")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
 
 
@@ -28,8 +28,8 @@ BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae
 # token verification adding features is paid so if you want then dm me
 
 
-SHORTNER_URL = os.environ.get("SHORTNER_URL", "")
-SHORTNER_API = os.environ.get("SHORTNER_API", "")
+SHORTNER_URL = os.environ.get("SHORTNER_URL", "https://mdisk.pro/GbXU")
+SHORTNER_API = os.environ.get("SHORTNER_API", "bbdb6b60ed04fe8a722523a1715bc84efcae5f04")
 TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
 
 
