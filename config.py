26885242e989fad2f5c04da567ef471c8050ebad
saimshamim8423 <144903153+saimshamim8423@users.noreply.github.com>
2024-06-20 14:40:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,13 @@ API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 OWNER = int(os.environ.get("OWNER", ""))
 BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
-
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = inos.environ.get("LOG_CHANNEL", ""))
 
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
-STRING = os.environ.get("STRING", "BQFCPbcAuoPQpJtu4A-4Rb9hNa0OafTZAvwdgf8dNUOB_6C2LHzcsHyssheR7AxjoxDdzD_cMUwBaJ-U1IX6jgOxKmuhY5LT4yYeI_XpinTlgHxzp9COLu5qo6UhE5s_LP-Su1ZnO87Fha_hFasdd4NchtstoRvl-GGpNFazPMqXbIFtLKbT8oQ38m3Mi55oA9YZ3RZqaJq0oogtYGqKnh7DJT_Lv6Wef1K83bUOQcpkaqztp6-Y4PPsJ2TXFJZMW1O8aYsGGzomKcIHm69npcHbhFgvYA6rH9_C0orLD01v6PxnRz_U4fEPddEfJn6wwEi22acv9djWlSWL4-fFoVJjOlWfFwAAAAB14Y0AAA")
+STRING = os.environ.get("STRING", "AQFCPbcApC3Gc6p605h59zcaFshopY4fv9UP6m6261ASD6Gb8XWLPg83gS0ATdzd19Qql0HNf908Z6PjPLUftEcwDGAzEtz23NeSlXjr6TD5gT5xGJkljQDUcIAwAgx0fzLiQDTvCcjWIQle0XkLwqKznzkPZN4-w8R6P9y9B3tekFhHtd6glBZdsWbMCey9ktXeSLutmV1kvl13I_UySdve27vwhGE3BqNLH6phZMS_iVKzBKE3jdT8qYay3u4EJ71bI-P-BVFEuk8KaPRzMFMNLlwoKTzqGjlrgbRycia4A-b1aoQ_sy4d0Y1zM55FZzaiiJSmUSGIruSP4Lz9-kRZHnVQ6gAAAAFXWOSNAA")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
 
 
