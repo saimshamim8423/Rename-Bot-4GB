f5677084637198405017e0edab9c11466ce13292
saimshamim8423 <144903153+saimshamim8423@users.noreply.github.com>
2024-06-22 18:21:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,20 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6636029469:AAFPSod7xidP2lnI4juZsrl0O7W1OjfgU7o")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6636029469")
 API_ID = int(os.environ.get("API_ID", "21118391"))
 API_HASH = os.environ.get("API_HASH", "43b91164d7825dc577c2baa7c6cdcee6")
 OWNER = int(os.environ.get("OWNER", "1977715968"))
 BOT_USERNAME = os.environ.get('BOT_USERNAME', "AnityRenamebot")
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "@Anity_managementbots")
-LOG_CHANNEL = inos.environ.get("LOG_CHANNEL", "-1002208160889"))
 
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://suryagupta1928:6thfnQ3AxzK6VJUA@cluster0.6ppqasw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "@Anity_managementbots")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002208160889"))
+
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Kakashi:aloksingh@kakashi.hnjnbw0.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
-STRING = os.environ.get("STRING", "BQFCPbcAmOrlu6evMUo1OnH8LGya2LYim9nbmvgsCiumxSLEb8IPpntxdxdcJMPV69M_fdexcvHCW-GJbeHSWZdCG0NltWAg2mgBhI913453qzZUlQTBumgw3pyWkgrUVE5odf31ZhKZC3MsI3uqKhKeKZ78hnLwBPFxSVI9fdhjgvpVxsgjvqkM3EaBKu9jIjDWgkPD0MzJLeo4BH0hxB9h-huHYBKoJTC3_rhexRwuq-9m3ZudPaVxhBmn4KHH86K01PPVjQrX3Ga1CGHkVHccqF_pqXHdarC-j_d7On3LOxSdkaAG7r8eHuSfyFrrxz_2j1rHN2On58jfSN1Lk3XmtcbGawAAAAGJgMuXAQ")
-BOT_PIC = os.environ.get("BOT_PIC", "https://telegra.ph/file/28bc91b86dfe16a45cae5.jpg")
+STRING = os.environ.get("STRING", "BQFCPbcAuoPQpJtu4A-4Rb9hNa0OafTZAvwdgf8dNUOB_6C2LHzcsHyssheR7AxjoxDdzD_cMUwBaJ-U1IX6jgOxKmuhY5LT4yYeI_XpinTlgHxzp9COLu5qo6UhE5s_LP-Su1ZnO87Fha_hFasdd4NchtstoRvl-GGpNFazPMqXbIFtLKbT8oQ38m3Mi55oA9YZ3RZqaJq0oogtYGqKnh7DJT_Lv6Wef1K83bUOQcpkaqztp6-Y4PPsJ2TXFJZMW1O8aYsGGzomKcIHm69npcHbhFgvYA6rH9_C0orLD01v6PxnRz_U4fEPddEfJn6wwEi22acv9djWlSWL4-fFoVJjOlWfFwAAAAB14Y0AAA")
+BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/507c658ba287e8d0206db.jpg")
 
 
 
@@ -21,15 +22,15 @@ BOT_PIC = os.environ.get("BOT_PIC", "https://telegra.ph/file/28bc91b86dfe16a45ca
 
 # if you need to add verify system then dm me on telegram
 # Check demo bots
-# https://t.me/FileRenameXBo
-# https://t.me/PremiumRenamerRobo
-# https://t.me/FileRenamerXRobo
+# https://t.me/FileRenameXBot
+# https://t.me/PremiumRenamerRobot
+# https://t.me/FileRenamerXRobot
 # token verification adding features is paid so if you want then dm me
 
 
-SHORTNER_URL = os.environ.get("SHORTNER_URL", "https://mdisk.pro/GbXU")
-SHORTNER_API = os.environ.get("SHORTNER_API", "bbdb6b60ed04fe8a722523a1715bc84efcae5f04")
-TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
+# SHORTNER_URL = os.environ.get("SHORTNER_URL", "")
+# SHORTNER_API = os.environ.get("SHORTNER_API", "")
+# TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
 
 
 # Jishu Developer 
