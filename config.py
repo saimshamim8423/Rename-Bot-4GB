bea9f04e87637a128d374a6fd181085e4d79164d
saimshamim8423 <144903153+saimshamim8423@users.noreply.github.com>
2024-06-22 18:03:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = inos.environ.get("LOG_CHANNEL", "-1002239343917"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6636029469:AAFPSod7xidP2lnI4juZsrl0O7W1OjfgU7o")
+API_ID = int(os.environ.get("API_ID", "21118391"))
+API_HASH = os.environ.get("API_HASH", "43b91164d7825dc577c2baa7c6cdcee6")
+OWNER = int(os.environ.get("OWNER", "1977715968"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "AnityRenamebot")
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "@Anity_managementbots")
+LOG_CHANNEL = inos.environ.get("LOG_CHANNEL", "-1002208160889"))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://suryagupta1928:6thfnQ3AxzK6VJUA@cluster0.6ppqasw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
 STRING = os.environ.get("STRING", "BQFCPbcAmOrlu6evMUo1OnH8LGya2LYim9nbmvgsCiumxSLEb8IPpntxdxdcJMPV69M_fdexcvHCW-GJbeHSWZdCG0NltWAg2mgBhI913453qzZUlQTBumgw3pyWkgrUVE5odf31ZhKZC3MsI3uqKhKeKZ78hnLwBPFxSVI9fdhjgvpVxsgjvqkM3EaBKu9jIjDWgkPD0MzJLeo4BH0hxB9h-huHYBKoJTC3_rhexRwuq-9m3ZudPaVxhBmn4KHH86K01PPVjQrX3Ga1CGHkVHccqF_pqXHdarC-j_d7On3LOxSdkaAG7r8eHuSfyFrrxz_2j1rHN2On58jfSN1Lk3XmtcbGawAAAAGJgMuXAQ")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
+BOT_PIC = os.environ.get("BOT_PIC", "https://telegra.ph/file/28bc91b86dfe16a45cae5.jpg")
 
 
 
