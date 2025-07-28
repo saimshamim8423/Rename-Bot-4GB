252d60aafafbf8ef16f0dc36fb0e47446253bf44
saimshamim8423 <144903153+saimshamim8423@users.noreply.github.com>
2025-07-28 22:09:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002208160889"))
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://Kakashi:aloksingh@kakashi.hnjnbw0.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQFCPbcAvNepTZP108dAFxzj1-ksGM643T5c5zyoc-G-VQ8o5Auq5Y5MfPaIXkj3K5wI34OPnmBcSCUPzdpNDWkSyyVTuP4V80hmoa0KlKxVFeiCNMsasI5q1iSNeQoAxFnnz2GkKe5Lu7l0PY-cv2766ACawifOgNI2wX3klNlIW-Um8Pi7dQKZUYw4NnBevTZyzehO15tOwd8dZp0F6FwpGT9cIRuQPAKe_2bu7V6xwSccQCbnXgXmBLV6fjAiPISyebgBOhF2a9p7CTwzhYeVuXi2VQmtmitdW1hLR43k3-QChl_INhX2TExZRzmKkf-fSQ75m8Qk68JaYlDnHuEyR8wI-AAAAAB14Y0AAA")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/507c658ba287e8d0206db.jpg")
 
 
